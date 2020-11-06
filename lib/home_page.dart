6897ae90cb069ddae542ac463b53795b77596044
Leuow <lmdl.eng@uea.edu.br>
2020-11-06 02:32:01 -0400

    insertion of the tabBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,36 +11,53 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.more_vert),
-              onPressed: () {},
-            )
-          ],
-        ),
-        body: ListView.separated(
-            itemCount: 3,
-            separatorBuilder: (context, index) => Divider(),
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://i.pinimg.com/originals/ba/e1/ee/bae1ee1a36e65ebcfc371cb880c4d2d1.jpg'),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green[900],
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              isScrollable: true,
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.camera_alt),
                 ),
-                title: Text('Russell Wilson $index'),
-              );
-            }),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.chat),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+                Tab(text: 'CONVERSAS'),
+                Tab(text: 'STATUS'),
+                Tab(text: 'CHAMADAS'),
+              ],
+            ),
+            title: Text(widget.title),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: () {},
+              )
+            ],
+          ),
+          body: ListView.separated(
+              itemCount: 3,
+              separatorBuilder: (context, index) => Divider(),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://i.pinimg.com/originals/ba/e1/ee/bae1ee1a36e65ebcfc371cb880c4d2d1.jpg'),
+                  ),
+                  title: Text('Russell Wilson $index'),
+                );
+              }),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.chat),
+            backgroundColor: Theme.of(context).accentColor,
+          ) // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+    );
   }
 }
